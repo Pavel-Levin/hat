52c18463aac8f1df07d3392a96c0a27b947b65d6
Pavel-Levin <60134945+Pavel-Levin@users.noreply.github.com>
2026-09-24 05:48:27 +0300

    Add Deye SG05 MQTT integration

---
+++ custom_components/deye_sg05_mqtt/const.py
@@ -0,0 +1,15 @@
+"""Constants for Deye SG05 MQTT."""
+from __future__ import annotations
+
+DOMAIN = "deye_sg05_mqtt"
+PLATFORMS = ["sensor"]
+
+DEFAULT_BROKER = "core-mosquitto"
+DEFAULT_PORT = 1883
+
+CONF_BROKER = "broker"
+CONF_PORT = "port"
+
+DEVICE_PREFIX = "id-nsg-v0.1-"
+MODEL = "SUN-20K-SG05LP3-EU-SM2"
+MANUFACTURER = "Deye"
